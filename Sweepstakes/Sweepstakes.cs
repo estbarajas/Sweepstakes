c505601a0cee08e40c538a17cbb0c885fa18c4aa
estbarajas <ebarajas@wolfmail.stritch.edu>
2018-07-11 10:59:31 -0500

    Sweeptakes class now contains, contestant register, picking winner, and printing contestant info functionality.

--- Sweepstakes/Sweepstakes.cs
+++ Sweepstakes/Sweepstakes.cs
@@ -9,24 +9,28 @@ namespace Sweepstakes
     class Sweepstakes
     {
         Dictionary<int, Contestant> registeredContestants;
+        string sweepstakeName;
         public Sweepstakes(string name)
         {
             registeredContestants = new Dictionary<int, Contestant>();
+            sweepstakeName = name;
         }
 
         public void RegisterContestant(Contestant contestant)
         {
-
+            registeredContestants.Add(contestant.registrationNumber,contestant);
         }
 
         public string PickWinner()
         {
-
+            Random random = new Random();
+            int randomNumber = random.Next(0, registeredContestants.Count);
+            return "Winner: " + registeredContestants[randomNumber].firstName + " " + registeredContestants[randomNumber].lastName + " " + registeredContestants[randomNumber].registrationNumber; 
         }
 
         public void PrintContestantInfo(Contestant contestant)
         {
-
+            Console.WriteLine(PickWinner());
         }
     }
 }
