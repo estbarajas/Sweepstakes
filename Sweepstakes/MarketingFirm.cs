157e5cea16069327fac19030d90e91af7232044a
estbarajas <ebarajas@wolfmail.stritch.edu>
2018-07-12 09:59:23 -0500

    Added the factory manager that returns either stack or queue.

--- Sweepstakes/MarketingFirm.cs
+++ Sweepstakes/MarketingFirm.cs
@@ -6,32 +6,12 @@ using System.Threading.Tasks;
 
 namespace Sweepstakes
 {
-    class MarketingFirm : IChooseStackOrQueue
+    class MarketingFirm 
     {
         ISweepstakesManager sweepstakesManager;
         public MarketingFirm(ISweepstakesManager sweepstakesManager)
         {
             this.sweepstakesManager = sweepstakesManager;
-        }
-
-        public void ChooseStackOrQueue(Sweepstakes sweepstake)
-        {
-            string stackOrQueue;
-            stackOrQueue = Console.ReadLine();
-            switch (stackOrQueue.ToLower())
-            {
-                case "stack":
-                    SweepstakesStackManager stackManager = new SweepstakesStackManager();
-                    stackManager.InsertSweepstakes(sweepstake);
-                    break;
-                case "queue":
-                    SweepstakesQueueManager queueManager = new SweepstakesQueueManager();
-                    queueManager.InsertSweepstakes(sweepstake);
-                    break;
-                default:
-                    Console.WriteLine("Invalid input.");
-                    break;
-            }           
-        }
+        }        
     }
 }
---
+++ Sweepstakes/SweepstakeManagerFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+     static class SweepstakeManagerFactory 
+    {
+        public static ISweepstakesManager ChooseStackOrQueue()
+        {
+            ISweepstakesManager chosenManager = null;
+            string stackOrQueue;
+            Console.WriteLine("What manager to use? \na) stack \nb) queue");
+            stackOrQueue = Console.ReadLine();
+            switch (stackOrQueue.ToLower())
+            {
+                case "a":
+                    SweepstakesStackManager stackManager = new SweepstakesStackManager();
+                    chosenManager = stackManager;
+                    break;
+                case "b":
+                    SweepstakesQueueManager queueManager = new SweepstakesQueueManager();
+                    chosenManager = queueManager;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input.");
+                    break;
+            }
+            return chosenManager;
+        }
+    }
+}
